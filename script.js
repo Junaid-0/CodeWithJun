8c0e94560bf85d8b3cb173f5fed759c5a6102d3e
Junaid-0 <Junaid-0>
2022-10-09 13:30:46 +0530

    Added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // new Audio("Audio/Rap-God.mp3").play()
+
+let flightbox = document.getElementsByClassName("services")[0];
+
+console.log(flightbox);
+
+flightbox.addEventListener("click", function(){
+    location.href = "https://akbartravels.com"
+})
