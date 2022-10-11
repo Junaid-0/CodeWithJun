801cfc1f688d382829943b26416d73160113c84c
Junaid-0 <Junaid-0>
2022-10-11 20:21:09 +0530

    Yes

--- script.js
+++ script.js
@@ -7,3 +7,18 @@
 // flightbox.addEventListener("click", function(){
 //     location.href = "https://akbartravels.com"
 // })
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2500); // Change image every 2 seconds
+}
